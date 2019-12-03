6d84f42203bb566ea1e6ebaff93db51320c6152d
Noah Elias Benkler <noah.benkler@gmx.de>
2019-12-03 02:16:31 +0100

    testing

--- deviceOrientation.js
+++ deviceOrientation.js
@@ -3,15 +3,15 @@ var win = window,
     doc = document,
     docElem = doc.documentElement,
     body = doc.getElementsByTagName('body')[0],
-    x = win.innerWidth || docElem.clientWidth || body.clientWidth,
+    x = win.innerWidth || docElem.clientWidth || body.clientWidth
 
 
 function handleOrientation(event) {
-    var bar = document.getElementById("bar");
+  var bar = document.getElementById("bar");
   var rotation = event.gamma;
-  document.getElementById("testing-rotation").innerHTML = "rotation is: " + rotation.toString();
-  bar.style.left =(x*rotation/90 - 40) + "px";
   var pixels = (x*rotation/90 - 40);
-  document.getElementById("testing-pixels").innerHTML = "pixels are: " + pixels.toString();
+  document.getElementById("testing-rotation").innerHTML = "rotations are:" + rotation.toString();
+  document.getElementById("testing-pixels").innerHTML = "pixels are:" + pixels.toString();
+  bar.style.left =(x*rotation/90 - 40) + "px";
 }
 window.addEventListener("deviceorientation", handleOrientation, true);
