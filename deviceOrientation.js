ad33f40e7e94215c48b544d63ab08f7557359935
Noah Elias Benkler <noah.benkler@gmx.de>
2019-12-03 01:26:06 +0100

    adding movement

--- deviceOrientation.js
+++ deviceOrientation.js
@@ -1,9 +1,15 @@
-window.addEventListener("deviceorientation", handleOrientation, true);
+
+var win = window,
+    doc = document,
+    docElem = doc.documentElement,
+    body = doc.getElementsByTagName('body')[0],
+    x = win.innerWidth || docElem.clientWidth || body.clientWidth,
+    y = win.innerHeight|| docElem.clientHeight|| body.clientHeight
+
 
 function handleOrientation(event) {
-  var absolute = event.absolute;
-  var alpha    = event.alpha;
-  var beta     = event.beta;
-  var gamma    = event.gamma;
-
+    var bar = document.getElementById("bar");
+  var beta = event.beta;
+  bar.style.left =(x*beta/180 - 40) + "px";
 }
+window.addEventListener("deviceorientation", handleOrientation, true);
