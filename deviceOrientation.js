810c803e732d7c3a470c74696cafe05c93fe4cbe
Noah Elias Benkler <noah.benkler@gmx.de>
2019-12-08 14:46:48 +0100

    adding boundries

--- deviceOrientation.js
+++ deviceOrientation.js
@@ -12,7 +12,7 @@ function handleOrientation(event) {
     document.getElementById("testing-new-position").innerHTML = "new position is:" + position;
     document.getElementById("testing-rotation").innerHTML = "rotations are:" + rotation;
     document.getElementById("testing-pixels").innerHTML = "pixels are:" + pixels;
-    if(position > 0 && (position) < (maxX) {
+    if(position > 0 && (position) < (maxX)) {
         bar.style.left = position + "px";
     }
 }
