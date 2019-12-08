b0fa67364c07502285ed9b47c3484e0d7e89608e
Noah Elias Benkler <noah.benkler@gmx.de>
2019-12-08 13:16:10 +0100

    fixing magical typo

--- deviceOrientation.js
+++ deviceOrientation.js
@@ -1,10 +1,12 @@
+
+
 function handleOrientation(event) {
     var bar = document.getElementById("bar");
     var rotation = event.gamma;
     var pixels = (x * rotation / 90);
     document.getElementById("testing-rotation").innerHTML = "rotations are:" + rotation;
     document.getElementById("testing-pixels").innerHTML = "pixels are:" + pixels;
-    if (doc.getElementsByTagName('body')[0].clientWidth > (x * rotation / 18 a0) && (x * rotation / 180) > 0)
+    if (doc.getElementsByTagName('body')[0].clientWidth > (x * rotation / 180) && (x * rotation / 180) > 0)
         bar.style.left = (x * rotation / 180) + "px";
 }
 window.addEventListener("deviceorientation", handleOrientation, true);
