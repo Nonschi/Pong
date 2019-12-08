f6862597cc55d4d1453cf375ef2f9b13593d36d5
Noah Elias Benkler <noah.benkler@gmx.de>
2019-12-08 13:41:36 +0100

    fixing brainfart

--- deviceOrientation.js
+++ deviceOrientation.js
@@ -7,10 +7,11 @@ function handleOrientation(event) {
     var rotation = event.gamma;
     var pixels = (maxX * rotation / 180);
     document.getElementById("clientWidth").innerHTML = "clientWidth is: " + maxX;
+    document.getElementById("testing-new-position") = "new position is:" + ()(maxX * rotation / 180)  + (maxX/2))
     document.getElementById("testing-rotation").innerHTML = "rotations are:" + rotation;
     document.getElementById("testing-pixels").innerHTML = "pixels are:" + pixels;
     if(pixles > 0 && pixels < maxX) {
-        bar.style.left = (maxX * rotation / 180)  + (maxX/2)+ "px";
+        bar.style.left = ()(maxX * rotation / 180)  + (maxX/2)) + "px";
     }
 }
 window.addEventListener("deviceorientation", handleOrientation, true);
