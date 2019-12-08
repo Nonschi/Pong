4024a68e8e6a8c5e851a26c690d2f0c16a93d6d8
Noah Elias Benkler <noah.benkler@gmx.de>
2019-12-08 14:11:47 +0100

    added everything back together

--- deviceOrientation.js
+++ deviceOrientation.js
@@ -7,12 +7,12 @@ function handleOrientation(event) {
     var maxX = document.getElementsByTagName('body')[0].clientWidth
     var rotation = event.gamma;
     var pixels = (maxX * rotation / 180);
-    var position = (maxX * rotation / 180)  + (maxX / 2) - 40;
+    var position = ((maxX * rotation) / 180) - 40;
     document.getElementById("clientWidth").innerHTML = "clientWidth is: " + maxX;
     document.getElementById("testing-new-position").innerHTML = "new position is:" + position;
     document.getElementById("testing-rotation").innerHTML = "rotations are:" + rotation;
     document.getElementById("testing-pixels").innerHTML = "pixels are:" + pixels;
-    if(position > 0 && position < maxX) {
+    if(position > 0 && (position + 80) < maxX) {
         bar.style.left = position + "px";
     }
 }
