5396ea657f37883e10e12e61c1281c4038845efd
Noah Elias Benkler <noah.benkler@gmx.de>
2019-12-08 16:05:50 +0100

    adding boundries

--- deviceOrientation.js
+++ deviceOrientation.js
@@ -5,18 +5,25 @@ function handleOrientation(event) {
 
     var bar = document.getElementById("bar");
     var maxX = document.getElementsByTagName('body')[0].clientWidth
+    var maxY = document.getElementsByTagName('body')[0].clientLength
     var boundry = maxX - 64;
     var rotation = event.gamma;
     var pixels = ((rotation / 90) * maxX)
     var position = ((rotation / 90) * maxX) + (maxX / 2)
-    document.getElementById("clientWidth").innerHTML = "clientWidth is: " + maxX;
-    document.getElementById("testing-new-position").innerHTML = "new position is:" + position;
-    document.getElementById("testing-rotation").innerHTML = "rotations are:" + rotation;
-    document.getElementById("testing-pixels").innerHTML = "pixels are:" + pixels;
     if(position > 0 && (position) < boundry) {
         bar.style.left = position + "px";
     }
 }
 
+function startGame() {
+    var maxX = document.getElementsByTagName('body')[0].clientWidth
+    var maxY = document.getElementsByTagName('body')[0].clientLength
+    var randomX = Math.random() * maxX;
+    while(randomX == 0) {
+        randomX = Math.random() * maxX;
+    }
+    document.getElementById("ball").style.left = randomX + "px";
+    document.getElementById("startGame").style.display = "none";
+}
 
 window.addEventListener("deviceorientation", handleOrientation, true);
