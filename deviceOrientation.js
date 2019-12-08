699d5c2f3d6ccc96f931c5b3f995ecc6d6c3866a
Noah Elias Benkler <noah.benkler@gmx.de>
2019-12-08 17:25:55 +0100

    adding boundries

--- deviceOrientation.js
+++ deviceOrientation.js
@@ -1,8 +1,37 @@
+var counter = 4;
+var interval;
+function timeIt() {
+    counter--;
+    console.log(counter);
+    document.getElementById("counter").innerHTML = counter;
+    if (counter == 0) {
+        clearInterval(interval);
+        test();
+    }
+}
+
+function startGame() {
+
+
+    document.getElementById("startGame").style.display = "none";
+    interval = setInterval(timeIt, 1000);
+}
+
+function test() {
+    var maxX = document.getElementsByTagName('body')[0].clientWidth
+    var maxY = document.getElementsByTagName('body')[0].clientLength
+    var randomX = Math.random() * maxX;
+    while(randomX == 0) {
+        randomX = Math.random() * maxX;
+    }
+    document.getElementById("ball").style.left = randomX + "px";
+    document.getElementById("bar").style.display = "inline";
+    document.getElementById("ball").style.display = "inline";
+    document.getElementById("counter").style.display = "none";
+}
 
 
 function handleOrientation(event) {
-
-
     var bar = document.getElementById("bar");
     var maxX = document.getElementsByTagName('body')[0].clientWidth
     var maxY = document.getElementsByTagName('body')[0].clientLength
@@ -15,15 +44,5 @@ function handleOrientation(event) {
     }
 }
 
-function startGame() {
-    var maxX = document.getElementsByTagName('body')[0].clientWidth
-    var maxY = document.getElementsByTagName('body')[0].clientLength
-    var randomX = Math.random() * maxX;
-    while(randomX == 0) {
-        randomX = Math.random() * maxX;
-    }
-    document.getElementById("ball").style.left = randomX + "px";
-    document.getElementById("startGame").style.display = "none";
-}
 
 window.addEventListener("deviceorientation", handleOrientation, true);
