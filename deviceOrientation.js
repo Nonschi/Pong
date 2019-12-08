b74c5b066df17a5f03d95de9aede01b22a0edb13
Noah Elias Benkler <noah.benkler@gmx.de>
2019-12-08 13:56:36 +0100

    removing parts

--- deviceOrientation.js
+++ deviceOrientation.js
@@ -6,7 +6,11 @@ function handleOrientation(event) {
     var rotation = event.gamma;
     var pixels = (maxX * rotation / 180);
     var position = (maxX * rotation / 180)  + (maxX / 2)
-    if(pixles > 0 && pixels < maxX) {
+    document.getElementById("clientWidth").innerHTML = "clientWidth is: " + maxX;
+    document.getElementById("testing-new-position") = "new position is:" + position;
+    document.getElementById("testing-rotation").innerHTML = "rotations are:" + rotation;
+    document.getElementById("testing-pixels").innerHTML = "pixels are:" + pixels;
+    if(pixels > 0 && pixels < maxX) {
         bar.style.left = position + "px";
     }
 }
